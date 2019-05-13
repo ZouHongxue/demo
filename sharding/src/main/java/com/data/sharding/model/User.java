e5db658fba51c04120ca8ff629820bedbc9055ef
coffee <hongxuezou@gmail.com>
2019-05-13 16:40:43 +0800

    use snowflake id

--- sharding/src/main/java/com/data/sharding/model/User.java
+++ sharding/src/main/java/com/data/sharding/model/User.java
@@ -6,7 +6,7 @@ import java.util.Date;
  * @author coffee
  */
 public class User {
-    private Integer id;
+    private Long id;
 
     private String userName;
 
@@ -20,11 +20,11 @@ public class User {
 
     private Date updatedAt;
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
