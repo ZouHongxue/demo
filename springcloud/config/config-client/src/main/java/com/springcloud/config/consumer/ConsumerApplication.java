5fc77d4b343b4552860bf723441459e9b0ffd7c3
coffee <hongxuezou@gmail.com>
2019-05-20 23:39:57 +0800

    Fix Spring Cloud Config Bug

--- springcloud/config/config-client/src/main/java/com/springcloud/config/consumer/ConsumerApplication.java
+++ springcloud/config/config-client/src/main/java/com/springcloud/config/consumer/ConsumerApplication.java
@@ -1,17 +1,23 @@
 package com.springcloud.config.consumer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class ConsumerApplication {
+
+    @Value("${hello}")
+    private String uri;
 
     @RequestMapping("/")
     public String home() {
-        return "hello world";
+        return "hello world" + uri;
     }
 
     public static void main(String[] args) {
